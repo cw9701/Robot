34cfd08f2bf9e085a853d7d3133b69e0f35b962c
woe <woe6933@gmail.com>
2024-04-04 11:11:17 -1200

    shooting and moving

--- 2024/DrivingRobot/src/main/java/frc/robot/Robot.java
+++ 2024/DrivingRobot/src/main/java/frc/robot/Robot.java
@@ -23,21 +23,26 @@ public class Robot extends TimedRobot {
   private CANSparkMax m_Shooter;
   private CANSparkMax m_Shooter_Follower;
 
-  private final DifferentialDrive m_robotDrive =
-      new DifferentialDrive(m_leftMotor::set, m_rightMotor::set);
-  private final XboxController m_driverController = new XboxController(0);
+  private DifferentialDrive m_robotDrive; 
+  private DifferentialDrive another_m_robotDrive; 
+  private XboxController m_driverController;
 
-  public Robot() {
-    SendableRegistry.addChild(m_robotDrive, m_leftMotor);
-    SendableRegistry.addChild(m_robotDrive, m_rightMotor);
+  public Robot() 
+  {
+
   }
 
   @Override
   public void robotInit() {
-    m_rightMotor = new CANSparkMax(1, MotorType.kBrushed);
-    m_rightFollower = new CANSparkMax(2, MotorType.kBrushed);
+
+    m_driverController =  new XboxController(0);
+    m_rightMotor = new CANSparkMax(2, MotorType.kBrushed);
+    m_rightFollower = new CANSparkMax(1, MotorType.kBrushed);
     m_leftMotor = new CANSparkMax(3, MotorType.kBrushed);
     m_leftFollower = new CANSparkMax(4, MotorType.kBrushed);
+
+    m_robotDrive = new DifferentialDrive(m_leftMotor::set, m_rightMotor::set);
+    another_m_robotDrive = new DifferentialDrive(m_leftFollower::set, m_rightFollower::set);
 
     m_Shooter = new CANSparkMax(5, MotorType.kBrushed);
     m_Shooter_Follower = new CANSparkMax(6, MotorType.kBrushed);
@@ -49,9 +54,11 @@ public class Robot extends TimedRobot {
 
     m_Shooter.restoreFactoryDefaults();
     m_Shooter_Follower.restoreFactoryDefaults();
+    SendableRegistry.addChild(m_robotDrive, m_rightMotor);
+    SendableRegistry.addChild(m_robotDrive, m_leftMotor);
 
-    m_rightFollower.follow(m_rightMotor);
-    m_leftFollower.follow(m_leftMotor);
+    // m_rightFollower.follow(m_rightMotor);
+    // m_leftFollower.follow(m_leftMotor);
 
     m_Shooter_Follower.follow(m_Shooter);
 
@@ -60,6 +67,10 @@ public class Robot extends TimedRobot {
     // gearbox is constructed, you might have to invert the left side instead.
     m_rightMotor.setInverted(true);
     m_rightFollower.setInverted(true);
+
+    m_leftMotor.setInverted(false);
+    m_leftFollower.setInverted(false);
+
   }
 
   @Override
@@ -67,7 +78,8 @@ public class Robot extends TimedRobot {
     // Drive with split arcade drive.
     // That means that the Y axis of the left stick moves forward
     // and backward, and the X of the right stick turns left and right.
-    m_robotDrive.arcadeDrive(-m_driverController.getLeftY(), -m_driverController.getRightX());
+    m_robotDrive.arcadeDrive(m_driverController.getLeftY(), m_driverController.getRightX());
+    another_m_robotDrive.arcadeDrive(m_driverController.getLeftY(), m_driverController.getRightX());
 
     if( m_driverController.getAButton() )
     {
@@ -75,7 +87,9 @@ public class Robot extends TimedRobot {
     }
     else if( m_driverController.getYButton() )
     {
-      m_Shooter.set(-0.5);
+      m_Shooter.set(-1
+      
+      );
     }
     else
     {
